59b3e71c500e26da5f27029ad2479ea547c1c6bd
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-03-13 21:40:30 +0900

    add command option

--- starter/src/main.rs
+++ starter/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap;
+extern crate python_starter;
+
+use clap::Clap;
+use std::io;
+use std::path::Path;
+
+#[derive(Clap)]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Python(Python),
+}
+
+#[derive(Clap)]
+struct Python {
+    #[clap(short, long)]
+    target_dir: String,
+}
+
+fn main() -> Result<(), io::Error> {
+    let opts: Opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::Python(t) => {
+            let target_dir = Path::new(&t.target_dir);
+            python_starter::setup(&target_dir)?;
+        }
+    }
+
+    Ok(())
 }
